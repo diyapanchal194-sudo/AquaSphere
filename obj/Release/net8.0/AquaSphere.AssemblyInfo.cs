3680b2e3467238a289677f934bb974fcfe4e1448
diyapanchal194-sudo <py116@live.mdx.ac.uk>
2026-03-18 23:12:39 +0000

    Refactored dependencies and configs to reflect new repo name

--- obj/Release/net8.0/AquaSphere.AssemblyInfo.cs
+++ obj/Release/net8.0/AquaSphere.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d343558477966887b668f5b90be6168f6fe37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a771574c9aa4367c83dfd3127bedd16a0ae0c3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

e4ad4d1a2df8762a96047d31028443a20b91948a
Clivesavio Fernandes <clivefernandes66035@gmail.com>
2026-03-25 17:05:03 +0000

    phase2

--- obj/Debug/net8.0/AquaSphere.AssemblyInfo.cs
+++ obj/Debug/net8.0/AquaSphere.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a771574c9aa4367c83dfd3127bedd16a0ae0c3ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9409b4dd6954f73732bb006eb471c5d23b32cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
